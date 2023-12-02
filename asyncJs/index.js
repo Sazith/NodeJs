e8f0c91016f4e049a50348031107c2689f9943c0
Sazith <saith.simecsoftdev@gmail.com>
2023-12-02 19:18:55 +0600

    callback function updated

--- asyncJs/index.js
+++ asyncJs/index.js
@@ -4,6 +4,12 @@ console.log("Line 1");
 
 getStudent(1,(student)=>{
     console.log(student);
+    getCourses(student,(courses)=>{
+        console.log(courses);
+        getQuizMarks(courses.courses,(marks)=>{
+            console.log(marks);
+        })
+    })
 })
 
 console.log('Line 2')
@@ -13,4 +19,20 @@ function getStudent (id,callback){
         console.log('Fetching from Dataase... ');
         callback({id:id, name:"Rahim"});
     }, 2000);
+}
+
+function getCourses(student,callback){
+    setTimeout(() => {
+        console.log('Student Courses from DataBase...');
+        callback({id:student.id,name:student.name,
+        courses:['Javascrtipt','Python']});
+
+    },2000);
+}
+
+function getQuizMarks(courses,callback){
+    setTimeout(() => {
+        console.log('Marks...');
+        callback({[courses[0]]:5,[courses[1]]:8})
+    }, 1500);
 }
